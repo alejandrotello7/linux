1dc7314efcb6309f27d6a0f3d4bab623dd898098
Alex Linux <alejandro.tello.f@gmail.com>
2022-02-01 12:54:44 +0100

    testing syscalls

--- arch/x86/summer/summer.c
+++ arch/x86/summer/summer.c
@@ -4,9 +4,14 @@
 		
 
 		
-SYSCALL_DEFINE2(summer,long *, arg1, long *, arg2){
-		
-	long sum = *arg1 + *arg2;
+SYSCALL_DEFINE2(summer,unsigned int, arg1, unsigned int, arg2){
+
+	int argument1 = 0;
+	int argument2 = 0;
+	pr_warn("from summer kernel");
+	copy_from_user((void *)argument1, (void *)arg1, sizeof(unsigned int));
+	copy_from_user((void *)argument2, (void *)arg2, sizeof(unsigned int));
+	long sum = argument1 + argument2;
 		
 	return sum;
 		
