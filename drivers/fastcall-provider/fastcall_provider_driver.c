dbe8c82ddcd0733359c474fa2144bc500a9bf920
Alex Linux <alejandro.tello.f@gmail.com>
2022-02-14 18:43:53 +0100

    modified ptr val

--- drivers/fastcall-provider/fastcall_provider_driver.c
+++ drivers/fastcall-provider/fastcall_provider_driver.c
@@ -76,8 +76,8 @@ static long register_function(unsigned long args)
 {
 	long result = 0;
 	struct ioctl_args *io_args;
-	io_args = kzalloc(sizeof(struct ioctl_args) + sizeof(char *(args->binary_code)), GFP_KERNEL);
-	if(copy_from_user(io_args->binary_code, char *(args->binary_code), sizeof(char *(args->binary_code))));
+	io_args = kzalloc(sizeof(struct ioctl_args) + sizeof((char *) args->binary_code), GFP_KERNEL);
+	if(copy_from_user(io_args->binary_code, (char *)args->binary_code, sizeof((char *)args->binary_code)));
 		result = 42;
 		goto fail_copy;
 
