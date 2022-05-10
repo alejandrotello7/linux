9453d095294e9cfb2cc4ae72fefe4543fcf8aac1
Alex Linux <alejandro.tello.f@gmail.com>
2022-05-10 11:28:05 +0200

    modified path

--- Documentation/fastcall-provider/copy_service.c
+++ Documentation/fastcall-provider/copy_service.c
@@ -16,11 +16,15 @@ int main(void)
 {
 	FILE *fptr1, *fptr2;
 	char c;
-    const char *library_path = "~/dev/summer-lib";
+    const char *library_path = "~/dev/summer-lib/";
     const char *library_name = "libsummermodule.so";
 	const char *function_name = "summer_module";
     const char *new_file = "test.txt";
     char temp_buffer[200];
+    char library_path_buffer[100];
+    char new_file_buffer[100];
+
+
     strcat(strcpy(temp_buffer, "objdump -d "), library_name);
     strcat(temp_buffer, " | awk -v RS= \'/^[[:xdigit:]]+ <");
     strcat(temp_buffer, function_name);
@@ -28,9 +32,11 @@ int main(void)
     strcat(temp_buffer, new_file);
     printf("%s\n", temp_buffer);
     system(temp_buffer);
+    strcat(strcpy(library_path_buffer,library_path), "test.txt");
+    strcat(strcpy(new_file_buffer,library_path), "new.txt");
 
-	fptr1 = fopen("./test.txt", "r");
-	fptr2 = fopen("./new.txt", "w");
+	fptr1 = fopen(library_path_buffer, "r");
+	fptr2 = fopen(new_file_buffer, "w");
 	int x = 0;
 	char temp[2];
 
